755f0f2881c64a198d5e0de93b2bfd55a86b9dba
Samiran2004 <samirans170@gmail.com>
2025-07-10 00:00:19 +0530

    add/New Habit Create

--- app/(tabs)/add-habit.jsx
+++ app/(tabs)/add-habit.jsx
@@ -1,12 +1,49 @@
-import { View, KeyboardAvoidingView, Platform } from 'react-native';
-import React from 'react';
-import { Button, SegmentedButtons, TextInput } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
 import { Image } from 'expo-image';
+import { useState } from 'react';
+import { Alert, KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native';
+import { Button, SegmentedButtons, TextInput } from 'react-native-paper';
+import { useAuth } from '../../lib/auth-context';
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from '../../lib/appwrite';
+import { ID } from 'react-native-appwrite';
+import { router } from 'expo-router';
 
 const FREQUENCIES = ["daily", "weekly", "monthly"];
 
 export default function AddHabit() {
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [frquency, setFrequency] = useState("daily");
+  const { user } = useAuth();
+
+  const handleSubmit = async () => {
+    try {
+      if (!user) {
+        return;
+      }
+
+      await databases.createDocument(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        ID.unique(),
+        {
+          user_id: user.$id,
+          title,
+          description,
+          frequency: frquency,
+          steak_count: 0,
+          last_completed: new Date().toISOString(),
+          createdAt: new Date().toISOString(),
+        }
+      );
+
+      router.back();
+    } catch (error) {
+      console.log("Error in create new habit: ", error.message);
+      Alert.alert("Error", "Error to create new habit.");
+    }
+  };
+
   return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -26,19 +63,26 @@ export default function AddHabit() {
       </View>
       <View style={styles.inner}>
 
-        <TextInput label="Title" mode="outlined" style={styles.input} />
-        <TextInput label="Description" mode="outlined" style={styles.input} />
+        <TextInput label="Title" mode="outlined" style={styles.input} onChangeText={setTitle} />
+        <TextInput label="Description" mode="outlined" style={styles.input} onChangeText={setDescription} />
 
         <SegmentedButtons
           buttons={FREQUENCIES.map((item) => ({
             value: item,
-            label: item.charAt(0).toUpperCase() + item.slice(1)
+            label: item.charAt(0).toUpperCase() + item.slice(1),
           }))}
           style={styles.segmentedButton}
+          onValueChange={(value) => setFrequency(value)}
+          value={frquency}
         />
 
+
         <View style={styles.buttonWrapper}>
-          <Button mode="contained" style={styles.addHabitButton}>
+          <Button mode="contained"
+            style={styles.addHabitButton}
+            disabled={!title || !description}
+            onPress={handleSubmit}
+          >
             Add Habit
           </Button>
         </View>
